54cc5bddb0d053d96ae457cba34df84d2de3d85b
Brandon Idun-Tawiah <88982788+brandonidun@users.noreply.github.com>
2025-05-29 19:22:46 +0000

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/test-utils',
+    '@nuxt/scripts',
+    '@nuxt/image',
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    '@nuxt/eslint',
+    '@nuxt/content'
+  ]
+})
